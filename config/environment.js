6038f33fc7b89f5b17aec59784219a56d149c84a
Trent Stromkins <trent@smallrobot.org>
2018-09-06 11:54:24 -0700

    api host

--- config/environment.js
+++ config/environment.js
@@ -7,7 +7,7 @@ module.exports = function(environment) {
     modulePrefix: 'smallrobotco',
     environment,
     rootURL: '/',
-    host: 'https://smallrobot.co', //drupal
+    host: '/api', //drupal
     namespace: 'api',
     locationType: 'router-scroll',
     historySupportMiddleware: true,
